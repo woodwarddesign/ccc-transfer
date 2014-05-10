adbf7755fd7f6a2196ea2627a2fd8732aff3879c
John Hannah <john@friendlymachine.net>
2014-05-09 20:09:29 -0400

    edits to templates, equal height column on sample college page, jquery edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,67 @@
 $(document).ready(function() {
 
+  // mix it up plugin
+  $(function(){
+    $('.view-school-search').mixItUp();
+  });
+  //vertiscroll plugin
+  $(function(){
+     $('.scrollable').vertiscroll();
+   });
+
+  //set up variable for mobile. set this to keep track of width so functions are run only on transition from
+  // moble to desktop and vice versa. if this isn't done, functions will fire constantly as window is resized
+  var mobile = 0;
+
+  // determine if width is 'mobile'
+  function setMobileValue() {
+    var viewport = $(window).width();
+    if (viewport < 970 && mobile === 0 ) {
+      mobile = 1;
+      editMobileMenu();
+      toggleMenu();
+    }
+    else if (viewport > 970 && mobile === 1 ) {
+      mobile = 0;
+    }
+  }
+
+  setMobileValue();
+
   $('body').addClass('js');
+
   // toggle menu
 
   // the toggle menu doesn't allow the first element to act as a link - it's instead a toggle.
   // the bit below copies the parent link and adds it to the child ul so that the page is accessible
-  var viewport = $(window).width();
-  if (viewport < 960 ) {
-    $links = $('li.menuparent').children('a:first-child');
-    if ($links.length > 0){
-      $links.each(function(i,link) {$(link).next().prepend($('<li></li>').append($(link).clone()))
-      })
-    }
+  function editMobileMenu() {
+      $links = $('li.menuparent').children('a:first-child');
+      if ($links.length > 0){
+        $links.each(function(i,link) {$(link).next().prepend($('<li></li>').append($(link).clone()))
+        })
+      }
   }
 
-  function toggleMenu() {
-    //get window width
-   var viewport = $(window).width();
-   if (viewport < 960 ) {
+  // equal height columns
+ //  function equalHeight(group) {
+ //      if (mobile ===0) {
+ //     tallest = 0;
+ //     group.each(function() {
+ //        thisHeight = $(this).height();
+ //        if(thisHeight > tallest) {
+ //           tallest = thisHeight;
+ //        }
+ //     });
+ //     group.height(tallest);
+ //   }
+ //   else {
+ //     $(".column").css('height', 'auto');
+ //   }
+ // }
 
+// equalHeight($(".column"));
+
+  function toggleMenu() {
       // add the toggle classes
 		  var $menu = $('.region-menu .menu'),
   	  $menulink = $('.menu-link'),
@@ -35,22 +78,11 @@ $(document).ready(function() {
   			var $this = $(this);
   			$this.toggleClass('active').next('ul').toggleClass('active');
   		});
-    }
   }
 
-    toggleMenu();
-
     $(window).resize(function() {
-      toggleMenu();
+      setMobileValue();
+      //equalHeight($(".column"));
     });
-
-    // mix it up plugin
-    $(function(){
-      $('.view-school-search').mixItUp();
-    });
-
-    $(function(){
-	     $('.scrollable').vertiscroll();
-     });
 
 		});
